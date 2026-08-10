72c9e75241b0d0802b6baad3f4c6bc61e1da632c
sundancezaf <zaf.aleman@gmail.com>
2026-08-10 10:22:40 -0400

    removed the vn

--- vuln-main.java
+++ vuln-main.java
@@ -7,20 +7,6 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 public class App
 {
 
-    static String secret = "secret";
-
-    private static void bad1() {
-        try {
-            // ruleid: java-jwt-hardcoded-secret
-            Algorithm algorithm = Algorithm.HMAC256("secret");
-            String token = JWT.create()
-                .withIssuer("auth0")
-                .sign(algorithm);
-        } catch (JWTCreationException exception){
-            //Invalid Signing configuration / Couldn't convert Claims.
-        }
-    }
-
     private static void ok1(String secretKey) {
         try {
             // ok: java-jwt-hardcoded-secret
@@ -35,25 +21,6 @@ public class App
 
     public static void main( String[] args )
     {
-        bad1();
         ok1(args[0]);
     }
-}
-
-abstract class App2
-{
-// ruleid: java-jwt-hardcoded-secret
-    static String secret = "secret";
-
-    public void bad2() {
-        try {
-            Algorithm algorithm = Algorithm.HMAC256(secret);
-            String token = JWT.create()
-                .withIssuer("auth0")
-                .sign(algorithm);
-        } catch (JWTCreationException exception){
-            //Invalid Signing configuration / Couldn't convert Claims.
-        }
-    }
-
 }
--- vulns/ssrf/ssrf.py
+++ vulns/ssrf/ssrf.py
@@ -1,4 +1,3 @@
-import urllib.request
 from flask import render_template
 
 
@@ -13,33 +12,9 @@ def ssrf_api(request, app):
 
     name = form['name']
     email = form['email']
-    original_picture_url = form['imageUrl']
-
-    downloaded_url = _download_image(original_picture_url, app)
 
     return render_template(
         'ssrf.html',
         email=email,
-        name=name,
-        original_url=original_picture_url,
-        profile_picture_url=downloaded_url
+        name=name
     )
-
-
-def _download_image(url, app):
-    if not url:
-        return ''
-
-    download_image_path = ''
-
-    with urllib.request.urlopen(url) as f:
-        download_image_path = f"{app.config['PUBLIC_UPLOAD_FOLDER']}/downloaded-image.png"
-
-        with open(download_image_path, 'wb') as file:
-            file_content = f.read()
-            file.write(file_content)
-            file.close()
-
-    public_url = f"{app.config['PUBLIC_UPLOADS_URL']}/downloaded-image.png"
-
-    return public_url
